6909f1a3abeb0d7155ed97db000b76d820ed5e4d
til <m15071452418@163.com>
2025-10-23 16:18:04 +0800

    add

---
+++ CakeToolset/Attribute/ReadOnlyPropertyAttribute.cs
@@ -0,0 +1,6 @@
+namespace CakeToolset.Attribute;
+
+[System.AttributeUsage(System.AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+public class ReadOnlyPropertyAttribute : System.Attribute {
+
+}
